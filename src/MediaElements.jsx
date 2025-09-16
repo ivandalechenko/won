6cfa9e560a9e174d5ffb53ccc7242cb7597219a9
ivandalechenko <ivandalechenko@gmail.com>
2025-09-16 12:03:00 +0300

    fix

--- src/MediaElements.jsx
+++ src/MediaElements.jsx
@@ -7,7 +7,7 @@ export default () => {
             <a href="https://www.dextools.io/app/en/token/usdwon" target='_blank'>
                 <img src="/dt.svg" alt="" />
             </a>
-            <a href="https://x.com/USDWON_TRX" target='_blank'>
+            <a href="https://x.com/usdwontron?s=21&t=8shy0WWt5b5TtmS5zHTzGg" target='_blank'>
                 <img src="/x.svg" alt="" />
             </a>
         </>
